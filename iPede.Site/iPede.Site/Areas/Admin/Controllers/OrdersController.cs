a56cf3fe3226eccf14868015f9377b35f146b49e
Yan Paulo <yanpaulo@hotmail.com>
2016-03-04 16:25:14 -0300

    Visualização de produtos na área de Admin

---
+++ iPede.Site/iPede.Site/Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using iPede.Site.Models.Entities;
+
+namespace iPede.Site.Areas.Admin.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private iPedeContext db = new iPedeContext();
+
+        // GET: Admin/Orders
+        public ActionResult Index()
+        {
+            var orders = db.Orders.Include(o => o.Status);
+            return View(orders.ToList());
+        }
+
+        // GET: Admin/Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Include("Items.Product").SingleOrDefault(item => item.OrderId == id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Create
+        public ActionResult Create()
+        {
+            throw new InvalidOperationException();
+            ViewBag.OrderStatusId = new SelectList(db.OrderStatuses, "OrderStatusId", "Name");
+            return View();
+        }
+
+        // POST: Admin/Orders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OrderId,OrderStatusId")] Order order)
+        {
+            throw new InvalidOperationException();
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.OrderStatusId = new SelectList(db.OrderStatuses, "OrderStatusId", "Name", order.OrderStatusId);
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            throw new InvalidOperationException();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.OrderStatusId = new SelectList(db.OrderStatuses, "OrderStatusId", "Name", order.OrderStatusId);
+            return View(order);
+        }
+
+        // POST: Admin/Orders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OrderId,OrderStatusId")] Order order)
+        {
+            throw new InvalidOperationException();
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.OrderStatusId = new SelectList(db.OrderStatuses, "OrderStatusId", "Name", order.OrderStatusId);
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            throw new InvalidOperationException();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Admin/Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            throw new InvalidOperationException();
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- iPede.Site/iPede.Site/Models/Entities/OrderStatus.cs
+++ iPede.Site/iPede.Site/Models/Entities/OrderStatus.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace iPede.Site.Models.Entities
     public class OrderStatus
     {
         public int OrderStatusId { get; set; }
+        [DisplayName("Status")]
         public string Name { get; set; }
     }
 }
