fdc1e3449326ad34093e1a54c31b054fed4e15a3
Yan Paulo <yanpaulo@hotmail.com>
2016-04-05 15:35:16 -0300

    Project stub with data access service created.

---
+++ iPede.WindowsApp/iPede.WindowsApp/iPede.WindowsApp/Service/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iPede.WindowsApp.Service
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        
+        public int CategoryId { get; set; }
+        
+        public int? MainImageId { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string ShortDescription { get; set; }
+        
+        public string FullDescription { get; set; }
+        
+        public decimal Price { get; set; }
+        
+        public string CategoryName { get; set; }
+    }
+}
---
+++ iPede.WindowsApp/iPede.WindowsApp/iPede.WindowsApp/Service/iPedeService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Web.Http;
+
+namespace iPede.WindowsApp.Service
+{
+    public class iPedeService
+    {
+        private HttpClient httpClient;
+        private Uri serviceUri;
+
+        public iPedeService()
+        {
+            httpClient = new HttpClient();
+            serviceUri = new Uri("http://localhost:58921/api/products/");
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            HttpResponseMessage response = await httpClient.GetAsync(serviceUri).AsTask();
+            var text = await response.Content.ReadAsStringAsync();
+
+            JsonArray jsResponse = JsonArray.Parse(text);
+            return jsResponse
+                .Where(o => o.ValueType == JsonValueType.Object)
+                .Select(o => FromJson(o.GetObject()));
+        }
+
+        private Product FromJson(JsonObject o)
+        {
+            return new Product
+            {
+                ProductId = (int)o.GetNamedNumber("ProductId"),
+                CategoryId = (int)o.GetNamedNumber("CategoryId"),
+                Name = o.GetNamedString("Name"),
+                ShortDescription = o.GetNamedString("ShortDescription"),
+                FullDescription = o.GetNamedString("FullDescription"),
+                Price = (decimal)o.GetNamedNumber("Price"),
+                CategoryName = o.GetNamedString("FullDescription")
+            };
+        }
+    }
+}
