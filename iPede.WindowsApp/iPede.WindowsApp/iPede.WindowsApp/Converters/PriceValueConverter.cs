c2c781b6bceb56219d2b9301b1d3843099f74bbd
Yan Paulo <yanpaulo@hotmail.com>
2016-04-06 16:27:30 -0300

    Created PriceValueConverted and applied it in ProductPage for it to do its job.

---
+++ iPede.WindowsApp/iPede.WindowsApp/iPede.WindowsApp/Converters/PriceValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace iPede.WindowsApp.Converters
+{
+    public class PriceValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            decimal str = (decimal)value;
+            return $"R$ {str:N2}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
