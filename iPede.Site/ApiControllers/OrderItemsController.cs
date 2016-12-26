623d397b5c117ca777e62b1c9656b883b3dc2f76
Yan Paulo <yanpaulo@hotmail.com>
2016-12-26 20:52:51 -0300

    Created NotificationManager, a class to send notifications. It deals with details and configurations that Controller classes must not be aware of.

--- iPede.Site/ApiControllers/OrderItemsController.cs
+++ iPede.Site/ApiControllers/OrderItemsController.cs
@@ -11,8 +11,6 @@ using System.Web.Http.Description;
 using iPede.Site.Models.Entities;
 using AutoMapper;
 using iPede.Site.Models.DTOs;
-using Microsoft.Azure.NotificationHubs;
-using System.Web.Configuration;
 using Newtonsoft.Json.Linq;
 using iPede.Site.Models;
 using System.Threading.Tasks;
@@ -99,7 +97,7 @@ namespace iPede.Site.ApiControllers
             //db.Entry(orderItem).Reference(oi => oi.Product).Load();
 
             var item = mapper.Map<OrderItemDTO>(orderItem);
-            await SendNotificationAsync(item);
+            await NotificationManager.Instance.SendNotificationAsync(NotificationEventNames.OrderItemCreated, item);
 
             return CreatedAtRoute("DefaultApi", new { id = item.Id }, item);
         }
@@ -133,36 +131,5 @@ namespace iPede.Site.ApiControllers
         {
             return db.OrderItems.Count(e => e.Id == id) > 0;
         }
-
-        private async Task SendNotificationAsync(OrderItemDTO item)
-        {
-            var appSettings = WebConfigurationManager.AppSettings;
-
-            NotificationHubClient hub = NotificationHubClient
-                .CreateClientFromConnectionString(appSettings["NotificationHubConnectionString"], appSettings["NotificationHubName"]);
-            //var toast = @"<toast><visual><binding template=""ToastText01""><text id=""1"">Hello from a .NET App!</text></binding></visual></toast>";
-            var jObject = JObject.FromObject(
-                new
-                {
-                    EventName = NotificationEventNames.OrderItemCreated,
-                    Item = item
-                });
-            var json = jObject.ToString();
-            try
-            {
-                var notification = new WindowsNotification(json)
-                {
-                    Headers = new Dictionary<string, string> { { "X-WNS-Type", "wns/raw" } }
-                };
-                var outcome = await hub.SendNotificationAsync(notification);
-                System.Diagnostics.Debug.WriteLine(outcome.State.ToString());
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine(ex.Message);
-                throw;
-            }
-        }
-
     }
 }
---
+++ iPede.Site/NotificationManager.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Azure.NotificationHubs;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+
+namespace iPede.Site
+{
+    public class NotificationManager
+    {
+        private static NotificationManager _instance;
+        private NotificationHubClient _hub;
+
+        private NotificationManager()
+        {
+            var appSettings = WebConfigurationManager.AppSettings;
+            _hub = NotificationHubClient
+                .CreateClientFromConnectionString(appSettings["NotificationHubConnectionString"], appSettings["NotificationHubName"]);
+        }
+
+        static NotificationManager()
+        {
+            _instance = new NotificationManager();
+        }
+
+        public static NotificationManager Instance => _instance;
+
+        public async Task SendNotificationAsync(string eventName, object item)
+        {
+            var jObject = JObject.FromObject(
+                new
+                {
+                    EventName = eventName,
+                    Item = item
+                });
+            var json = jObject.ToString();
+            try
+            {
+                var windowsNotification = new WindowsNotification(json)
+                {
+                    Headers = new Dictionary<string, string> { { "X-WNS-Type", "wns/raw" } }
+                };
+                var outcome = await _hub.SendNotificationAsync(windowsNotification);
+                System.Diagnostics.Debug.WriteLine(outcome.State.ToString());
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+    }
+}
