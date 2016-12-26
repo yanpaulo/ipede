66ea1ea958a68679add2aaf09ee17fc4f1638f31
Yan Paulo <yanpaulo@hotmail.com>
2016-12-26 12:28:20 -0300

    Updated Web Service to support sending notifications through Azure Notification Hub. Updated Web Service to send notifications to all clients when an OrderItem is created (POSTed by a client).

--- iPede.Site/ApiControllers/OrderItemsController.cs
+++ iPede.Site/ApiControllers/OrderItemsController.cs
@@ -11,6 +11,11 @@ using System.Web.Http.Description;
 using iPede.Site.Models.Entities;
 using AutoMapper;
 using iPede.Site.Models.DTOs;
+using Microsoft.Azure.NotificationHubs;
+using System.Web.Configuration;
+using Newtonsoft.Json.Linq;
+using iPede.Site.Models;
+using System.Threading.Tasks;
 
 namespace iPede.Site.ApiControllers
 {
@@ -82,7 +87,7 @@ namespace iPede.Site.ApiControllers
 
         // POST: api/OrderItems
         [ResponseType(typeof(OrderItem))]
-        public IHttpActionResult PostOrderItem(OrderItem orderItem)
+        public async Task<IHttpActionResult> PostOrderItem(OrderItem orderItem)
         {
             if (!ModelState.IsValid)
             {
@@ -91,8 +96,12 @@ namespace iPede.Site.ApiControllers
 
             db.OrderItems.Add(orderItem);
             db.SaveChanges();
-            db.Entry(orderItem).Reference(oi => oi.Product).Load();
-            return CreatedAtRoute("DefaultApi", new { id = orderItem.Id }, mapper.Map<OrderItemDTO>(orderItem));
+            //db.Entry(orderItem).Reference(oi => oi.Product).Load();
+
+            var item = mapper.Map<OrderItemDTO>(orderItem);
+            await SendNotificationAsync(item);
+
+            return CreatedAtRoute("DefaultApi", new { id = item.Id }, item);
         }
 
         // DELETE: api/OrderItems/5
@@ -124,5 +133,36 @@ namespace iPede.Site.ApiControllers
         {
             return db.OrderItems.Count(e => e.Id == id) > 0;
         }
+
+        private async Task SendNotificationAsync(OrderItemDTO item)
+        {
+            var appSettings = WebConfigurationManager.AppSettings;
+
+            NotificationHubClient hub = NotificationHubClient
+                .CreateClientFromConnectionString(appSettings["NotificationHubConnectionString"], appSettings["NotificationHubName"]);
+            //var toast = @"<toast><visual><binding template=""ToastText01""><text id=""1"">Hello from a .NET App!</text></binding></visual></toast>";
+            var jObject = JObject.FromObject(
+                new
+                {
+                    EventName = NotificationEventNames.OrderItemCreated,
+                    Item = item
+                });
+            var json = jObject.ToString();
+            try
+            {
+                var notification = new WindowsNotification(json)
+                {
+                    Headers = new Dictionary<string, string> { { "X-WNS-Type", "wns/raw" } }
+                };
+                var outcome = await hub.SendNotificationAsync(notification);
+                System.Diagnostics.Debug.WriteLine(outcome.State.ToString());
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
     }
 }
---
+++ iPede.Site/Models/NotificationEventNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iPede.Site.Models
+{
+    public static class NotificationEventNames
+    {
+        public static readonly string
+            OrderCreated = "ORDER_CREATED",
+            OrderItemCreated = "ORDER_ITEM_CREATED";
+    }
+}
